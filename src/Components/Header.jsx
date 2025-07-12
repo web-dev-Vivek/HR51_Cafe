cea41df94e8599406d736f8d2caf89120eeaa89f
Vivek Jha <progamervivek2020@gmail.com>
2025-07-12 09:22:05 +0530

    .

---
+++ src/Components/Header.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <div>
+        <div>
+          <img src="" alt="" />
+        </div>
+        <div>
+          <div>
+            <img src="" alt="" />
+            <p>Home</p>
+          </div>
+          <div>
+            <img src="" alt="" />
+            <p>Menu</p>
+          </div>
+          <div>
+            <img src="" alt="" />
+            <p>About us</p>
+          </div>
+          <div>
+            <img src="" alt="" />
+            <p>Contact</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
