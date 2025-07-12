d7c9c9026aa91a1f8bba082e06584769b811a855
Vivek Jha <progamervivek2020@gmail.com>
2025-07-12 11:38:02 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Home from "./Pages/Home.jsx";
 
 function App() {
-  return <></>;
+  return (
+    <div className="App ">
+      <Home />
+    </div>
+  );
 }
 
 export default App;
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,28 +1,60 @@
 import React from "react";
+import About from "../assets/about.png";
+import Logo from "../assets/logo.png";
+import Contact from "../assets/contact.png";
+import Chef from "../assets/chef.png";
+import Menu from "../assets/menu.png";
+import Arrow from "../assets/arrow.png";
+import Home from "../assets/home.png";
 
 function Header() {
   return (
     <div>
-      <div>
-        <div>
-          <img src="" alt="" />
+      <div className="w-full h-full flex justify-between items-center bg-white p-4 bg-red-600">
+        <div className="h-auto">
+          <h1
+            style={{ fontFamily: "Bubblegum Sans", cursive: "cursive" }}
+            className="font-bold text-4xl h-auto"
+          >
+            HR-51
+          </h1>
         </div>
-        <div>
+        <div className="flex items-center gap-4">
           <div>
-            <img src="" alt="" />
-            <p>Home</p>
+            <img className="w-6 h-6" src={Home} alt="" />
+            <p
+              style={{ fontFamily: "Bubblegum Sans", cursive: "cursive" }}
+              className="text-lg"
+            >
+              Home
+            </p>
           </div>
           <div>
-            <img src="" alt="" />
-            <p>Menu</p>
+            <img className="w-6 h-6" src={Menu} alt="" />
+            <p
+              style={{ fontFamily: "Bubblegum Sans", cursive: "cursive" }}
+              className="text-lg"
+            >
+              Menu
+            </p>
           </div>
           <div>
-            <img src="" alt="" />
-            <p>About us</p>
+            <img className="w-6 h-6" src={About} alt="" />
+            <p
+              style={{ fontFamily: "Bubblegum Sans", cursive: "cursive" }}
+              className="text-lg"
+            >
+              About us
+            </p>
           </div>
           <div>
-            <img src="" alt="" />
-            <p>Contact</p>
+            <img className="w-6 h-6" src={Contact} alt="" />
+            <p
+              style={{ fontFamily: "Bubblegum Sans", cursive: "cursive" }}
+              className="text-lg"
+            >
+              Contact
+            </p>
           </div>
         </div>
       </div>
